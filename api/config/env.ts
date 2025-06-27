d4bf588c31e87345eaa1e5211dc9bd6907115be6
Zeus Elderfield <elderfieldzeus24@gmail.com>
2025-06-27 16:55:31 +0800

    chore: init

--- api/config/env.ts
+++
@@ -1,5 +0,0 @@
-const env = {
-    JWT_SECRET: process.env.JWT_SECRET || 'your_jwt_secret',
-}
-
-export default env;
--- api/routes/user.route.ts
+++
@@ -1,6 +0,0 @@
-import { Hono } from "hono";
-import { signup } from "../controllers/user.controller";
-
-export const userRouter = new Hono();
-
-userRouter.post("/signup", signup);
